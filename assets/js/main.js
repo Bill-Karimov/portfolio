f7bc8d89b53dcc4558ca238ebb1346c52f5f73c6
Bill-Karimov <Xayriddinovich@gmail.com>
2022-01-03 17:57:43 +0500

    Добавлен кастомный курсов

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,62 @@
      } , 500 , 'easeInOutCubic' );  
 } )
  */
-     $(document).ready(function($) {
-          console.log('a');
-     });
-     var cursor = $('#cursor');
-     $(document).bind('mousemove', function(event) {
 
-          var x = e.clientX;
-          var y = e.clientY;
-          console.log(x);
-          cursor.style.left = x + 'px';
-          cursor.style.left = y + 'px';
-     });
-          
+$(document).mousemove(function(e) {
+     mouseCoords(e);
+
+});
+
+const cursor = $('#cursor');
+const aura = $('#aura');
+const links = $('a');
+
+mouseX = 0, mouseY = 0, posX = 0, posY = 0;
+
+function mouseCoords (e) {
+     mouseX = e.pageX
+     mouseY = e.pageY
+}
+
+gsap.to({}, .01, {
+     repeat: -1,
+
+     onRepeat: () => {
+
+          posX += (mouseX - posX) / 5
+          posY += (mouseY - posY) / 5
+
+          gsap.set(cursor, {
+               css: {
+                    left: mouseX,
+                    top: mouseY
+               }
+          });
+
+          gsap.set(aura, {
+               css: {
+                    left: posX - 26,
+                    top: posY - 26
+               }
+          });
+
+     }
+});
+
+$(document).ready(function($) {
+
+     for (let i = 0; i < links.length; i++) {
+          links[i].addEventListener('mouseover', () => {
+               cursor.addClass('cursor-active');
+               aura.addClass('cursor-active');
+          });
+     }
+     for (let i = 0; i < links.length; i++) {
+          links[i].addEventListener('mouseout', () => {
+               cursor.removeClass('cursor-active');
+               aura.removeClass('cursor-active');
+          });
+     }
+     
+
+});
